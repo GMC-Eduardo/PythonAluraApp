fa7e3ead58312b924701f228fe3bdb726c789bb2
GMC-Eduardo <gmc.eduardo@outlook.com>
2024-03-20 17:34:19 -0300

    readjusted to single commas

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -7,10 +7,10 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
 """)
 
-print("1. Cadastra restaurante")
-print("2. Listar restaurante")
-print("3. Ativar restaurante")
-print("4. Sair cadastro\n")
+print('1. Cadastra restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair cadastro\n')
 
-opcao_escolhida = input("Escolha uma opção: ")
+opcao_escolhida = input('Escolha uma opção: ')
 print(f'Você escolheu a opção {opcao_escolhida}')
