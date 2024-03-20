67675ca22e1a53af7c5d556b0206a67ce02665c5
Edu Back <backtestedu@gmail.com>
2024-03-20 16:31:26 -0300

    Switched to double quotes

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,9 +1,9 @@
-print('Sabor Express\n')
+print("Sabor Express\n")
 
-print('1. Cadastra restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair cadastro\n')
+print('"1. Cadastra restaurante")
+print('"2. Listar restaurante")
+print('"3. Ativar restaurante")
+print('"4. Sair cadastro\n")
 
-opcao_escolhida = input('Escolha uma opção: ')
-print('Você escolheu a opção', opcao_escolhida)
+opcao_escolhida = input("Escolha uma opção: ")
+print("Você escolheu a opção", opcao_escolhida)
